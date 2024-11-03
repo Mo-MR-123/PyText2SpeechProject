fbf42ddd12505d691359b900594c404dbf068215
unknown <43928182+Mo-MR-123@users.noreply.github.com>
2024-11-03 13:28:23 +0100

    added debug flag for when verbose warnings need to be studied

--- run_main.sh
+++ run_main.sh
@@ -8,4 +8,7 @@ export PYTHONPATH="$PWD"
 export IS_DEV_LOGGING=1 # log to stdout if we are in dev. otherwise log in files.
 export ENABLE_LOGGING=0
 
+# Set DEBUG_FLAG for debugging (1 for debugging, unset or 0 for normal execution)
+export DEBUG_FLAG=1  # Change to 0 or comment out to run without debugging
+
 python src/main.py
--- src/main.py
+++ src/main.py
@@ -1,20 +1,26 @@
-import torch
+# import torch
+import os
+import warnings
 
-from pathlib import Path
-from TTS.api import TTS
-from src.text_processors.str_to_sentences import split_into_sentences
-from src.parsers.pypdf_parser import PyPdfParser
-from src.text_processors.pre_processor import PreProcessor
+# Suppress all warnings since this is a GUI app
+# Warnings present during debugging only
+if not os.getenv("DEBUG_FLAG", default=0):
+    warnings.filterwarnings("ignore")
+
+# from pathlib import Path
+# from TTS.api import TTS
+# from src.text_processors.str_to_sentences import split_into_sentences
+# from src.parsers.pypdf_parser import PyPdfParser
+# from src.text_processors.pre_processor import PreProcessor
 from ui.tk_window_main import TkWindowMain
-from tkinter import Tk
+# from tkinter import Tk
 # from src.parsers.paddle_ocr_parser import PaddleOCRParser
 
 # TODO: check a way to disable logging centrally
 if __name__ == "__main__":
     TkWindowMain("test title").run_mainloop()
-    exit(0)
 
-    pre_processor = PreProcessor()
+    # pre_processor = PreProcessor()
 
     # TXT FROM PDF
     # pdf_parser = PdfParser("path/to/pdf", post_processor=post_processor)
@@ -25,16 +31,16 @@ if __name__ == "__main__":
     # ocr = PaddleOCRParser(post_processor=post_processor)
     # result = ocr.local_img_to_txt(Path("test.png"))
     
-    result = """ 
-    """
+    # result = """ 
+    # """
 
-    res_sentences_splitted = split_into_sentences(result)
-    print(f"{res_sentences_splitted=}")
+    # res_sentences_splitted = split_into_sentences(result)
+    # print(f"{res_sentences_splitted=}")
 
     # remove punctuations from sentences so that voice generations
     # is less likely to contain gibberish noise, repeating or unintelligible sound
-    res_sentences_no_punctuations = pre_processor.remove_punctuations(res_sentences_splitted)
-    print(f"{res_sentences_no_punctuations=}")
+    # res_sentences_no_punctuations = pre_processor.remove_punctuations(res_sentences_splitted)
+    # print(f"{res_sentences_no_punctuations=}")
 
     # res_sentences = 'asdasdqweqwe'
     # IMG TO TXT
@@ -45,18 +51,18 @@ if __name__ == "__main__":
     # result = ocr.local_img_to_txt(img_path, _cls=False)
 
     # TTS
-    dev = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"{dev=}")
+    # dev = "cuda" if torch.cuda.is_available() else "cpu"
+    # print(f"{dev=}")
 
     # TODO: https://docs.coqui.ai/en/dev/models/xtts.html --> check this site for more info about XTTS_v2
-    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(dev)
-    tts.tts_to_file(text=res_sentences_no_punctuations, speaker_wav=[
-        # "audio_cindy_nl_sample1.wav", # TODO: longer samples of voice do not necessarily lead to better output. It also decreases performance.
-        # "audio_cindy_nl_sample2.wav",
-        # "audio_cindy_nl_sample3.wav",
-        # "audio_cindy_nl_sample4.wav",
-        "audio_cindy_nl_sample5_short.wav",
-        "audio_cindy_nl_sample6_short.wav",
-        "audio_cindy_nl_sample7_short.wav",
-        "audio_cindy_nl_sample8_short.wav",
-    ], language="nl", file_path="test.wav", split_sentences=False)
+    # tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(dev)
+    # tts.tts_to_file(text=res_sentences_no_punctuations, speaker_wav=[
+    #     # "audio_cindy_nl_sample1.wav", # TODO: longer samples of voice do not necessarily lead to better output. It also decreases performance.
+    #     # "audio_cindy_nl_sample2.wav",
+    #     # "audio_cindy_nl_sample3.wav",
+    #     # "audio_cindy_nl_sample4.wav",
+    #     "audio_cindy_nl_sample5_short.wav",
+    #     "audio_cindy_nl_sample6_short.wav",
+    #     "audio_cindy_nl_sample7_short.wav",
+    #     "audio_cindy_nl_sample8_short.wav",
+    # ], language="nl", file_path="test.wav", split_sentences=False)
